257d242dd22ebbc40731e7b02908dd8f64795e21
Nir <niradler55@gmail.com>
2018-02-23 19:05:33 +0200

    add mock data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const fs = require('fs')
 const search = require('./controllers/github/search')
 const events = require('./controllers/github/events')
 
@@ -33,12 +34,54 @@ app.get('/github/events', async(req, res) => {
         return res.json({old_etag:etag,etag:events_res.headers.etag,events:events_res.data});
     } catch (error) {
         console.log(error)
+        try { // send mock
+            const events_res = await restore_from_local('none')
+            return res.status(403).json({old_etag:req.query.etag,etag:req.query.etag,events:events_res.data});
+        } catch (error) {
+            console.log(error) 
+        }
         return res
             .status(500)
             .json({msg: error.message});
     }
 
 })
+
+const restore_from_local = (etag) => {
+    return new Promise((resolve, reject) => {
+        try {
+            fs
+            .readFile(`./data/${etag}.json`, 'utf8', function (err, data) {
+                if (err) 
+                    reject(err)
+                    else
+                resolve(JSON.parse(data))
+            });  
+        } catch (error) {
+            reject(error) 
+        }
+
+    })
+}
+
+const save_local = (etag, data) => {
+    return new Promise((resolve, reject) => {
+        try {
+            fs
+            .writeFile(`./data/${etag}.json`, JSON.stringify(data), function (err) {
+                if (err) {
+                    console.log(err);
+                    reject(err)
+                }
+                console.log("The file was saved!");
+                resolve("The file was saved!")
+            }); 
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('Example app listening on port ' + port))
